e7ff07173bad5556295e82ee34b854f263c13353
Tripthi <tripthishetty@Tripthis-MBP.verizon.net>
2019-05-08 16:24:28 -0400

    add js file

---
+++ nightwatch.js
@@ -0,0 +1,17 @@
+
+
+//require('/Users/Sukad/Desktop/Nightwatch/nightwatch/bin/runner.js');
+
+//require('/Users/sukada/Desktop/Nightwatch/nightwatch/bin/runner.js');
+
+require('/Users/tripthishetty/Desktop/Nightwatch/nightwatch/bin/runner.js');
+
+
+
+//require('/Users/tripthi/Desktop/Nightwatch/nightwatch/bin/runner.js');
+
+
+
+
+
+
