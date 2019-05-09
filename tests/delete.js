215f02796c2ab4b34114a83fb22f221665268e60
tripthilohit <tripthilohit@gmail.com>
2019-05-09 17:18:58 -0400

    smallest number with user input

---
+++ tests/delete.js
@@ -0,0 +1,2 @@
+var prompt = require('prompt-sync')();
+var n = prompt('How many more times? ');
--- tests/test3.js
+++ tests/test3.js
@@ -1,18 +1,21 @@
 //Generate 500 random numbers and print nth smallest number 
 
+var prompt = require('prompt-sync')();
+
 var smallestNumber = function(num)
 {
+	
 	var a=[]
 	var temp
-	for(var i=0;i<10;i++)
+	for(var i=0;i<num;i++)
 	{
 		//a[i]= Math.random()
 		a[i]=Math.floor(Math.random() * 100000);
 		console.log("random numbers: "+a[i])
 	}
-	for (var j=0;j<10;j++)
+	for (var j=0;j<num;j++)
 	{
-		for(var k=0;k<10;k++)
+		for(var k=0;k<num;k++)
 		{
 			if(a[j]>a[k])
 			{
@@ -25,7 +28,10 @@ var smallestNumber = function(num)
 		}
 		
 	}
-return a[num]
+return a[num-1]
+
 }
 
-console.log(smallestNumber(9))
+
+var n = prompt('How many more times? ');
+console.log(smallestNumber(n))
